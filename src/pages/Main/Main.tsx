908d46ab4e86446d9d053fc4928dc93f60b6ff29
SangHoon <joa3668@gmail.com>
2022-02-17 22:07:41 +0900

    Add: json 데이터 변환후 추가, prettier eslint 관련 모듈 추가

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,7 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import * as S from './Main.style';
 
 function Main() {
+  const [data, setData] = useState();
+
+  const getData = async () => {
+    const json = await (await fetch('/data/mockData.json')).json();
+    setData(json.result);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  console.log(data);
+
   return <div>Main</div>;
 }
 
